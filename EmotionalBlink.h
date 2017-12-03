104c45c4111ae6f699ee6e492a13baa689070aa8
pokio <32353873+pokiiio@users.noreply.github.com>
2017-12-03 17:28:48 +0900

    Have a happy holiday season!

--- EmotionalBlink.h
+++ EmotionalBlink.h
@@ -7,14 +7,17 @@
 #include "WProgram.h"
 #endif
 
-class EmotionalBlink {
-  public:
-    void softly(Adafruit_NeoPixel* neopixel, int numLed, int r, int g, int b);
-    void softly(Adafruit_NeoPixel* neopixel, int numLed, int r, int g, int b, int intervalMsec);
-    void likePartyPeople(Adafruit_NeoPixel* neopixel, int numLed, int durationMsec);
-  private:
-    void setColor(Adafruit_NeoPixel* neopixel, int numLed, int r, int g, int b);
-    void setRandomColor(Adafruit_NeoPixel* neopixel, int numLed);
+class EmotionalBlink
+{
+public:
+  void softly(Adafruit_NeoPixel *neopixel, int numLed, int r, int g, int b);
+  void softly(Adafruit_NeoPixel *neopixel, int numLed, int r, int g, int b, int intervalMsec);
+  void likePartyPeople(Adafruit_NeoPixel *neopixel, int numLed, int durationMsec);
+  void likeHolidaySeason(Adafruit_NeoPixel *neopixel, int numLed, int durationMsec);
+
+private:
+  void setColor(Adafruit_NeoPixel *neopixel, int numLed, int r, int g, int b);
+  void setRandomColor(Adafruit_NeoPixel *neopixel, int numLed);
 };
 
 extern EmotionalBlink Blink;
